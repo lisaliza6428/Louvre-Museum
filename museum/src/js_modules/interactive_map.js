f1517c62c9ae675ebf26061c2f1cb04740b60f43
Elizaveta Ivanushenko <elizavetaivanushenko@gmail.com>
2021-10-04 22:07:13 +0300

    feat: fix styles

---
+++ museum/src/js_modules/interactive_map.js
@@ -0,0 +1,17 @@
+/* 
+mapboxgl.accessToken = 'pk.eyJ1IjoibGlzYWxpemE2NDI4IiwiYSI6ImNrdWNrMW9hajB6NXYzMW12Ymt5M2NuZ3MifQ.T4b2--uZ14J9xjMtZtk8mg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+
+// Create a default Marker and add it to the map.
+const marker1 = new mapboxgl.Marker()
+.setLngLat([12.554729, 55.70651])
+.addTo(map);
+ 
+// Create a default Marker, colored black, rotated 45 degrees.
+const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+.setLngLat([12.65147, 55.608166])
+.addTo(map); */
