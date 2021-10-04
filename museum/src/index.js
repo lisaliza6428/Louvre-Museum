73c2fd890b1d91dcfce6bde359a0a015f2053ce3
Elizaveta Ivanushenko <elizavetaivanushenko@gmail.com>
2021-10-04 10:12:04 +0300

    feat: add burgermenu

--- museum/src/index.js
+++ museum/src/index.js
@@ -4,3 +4,22 @@ import './js_modules/gallery_section'
 import './js_modules/buy_tickets'
 
 
+/* BURGER */
+
+
+
+const menuIcon = document.querySelector('.burger');
+const menuIconClose = document.querySelector('.burger_close');
+const menuNav = document.querySelector('.header_navigation');
+const welcomeContent = document.querySelector('.welcome_content');
+
+
+function toggleMenu() {
+  menuNav.classList.toggle('burger_open');
+  menuIcon.classList.toggle('burger_none');
+  menuIconClose.classList.toggle('burger_close_active');
+  welcomeContent.classList.toggle('welcome_invisible')
+}
+
+menuIcon.addEventListener('click', toggleMenu);
+menuIconClose.addEventListener('click', toggleMenu);
