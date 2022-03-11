6e9b43ca829d59af3c1278a0d7e5b50ea228d800
Elizaveta <elizavetaivanushenko@gmail.com>
2022-03-11 16:00:52 +0300

    refactor: change file structure

--- museum/src/components/tickets-section/form.js
+++ museum/src/components/tickets-section/form.js
@@ -1,4 +1,57 @@
 import './form.scss'
+
+import './tickets.scss'
+
+/* RIPPLE */
+const btn = document.querySelector(".btn_ripple");
+const ripple = document.querySelector(".ripple");
+
+const pointerdown = ({ clientX, clientY }) => {
+  const rect = btn.getBoundingClientRect();
+
+  ripple.style.top = `${clientY - rect.top - 150}px`;
+  ripple.style.left = `${clientX - rect.left - 150}px`;
+  ripple.classList.add("ripple-active");
+};
+const pointerup = () => {
+  setTimeout(() => {
+    ripple.classList.remove("ripple-active");
+  }, 500);
+};
+
+btn.addEventListener("mousedown", pointerdown);
+btn.addEventListener("touchstart", pointerdown);
+btn.addEventListener("mouseup", pointerup);
+btn.addEventListener("touchend", pointerup);
+
+
+
+/* FORM */
+let overlay = document.querySelector('.overlay');
+let form = document.querySelector('.form');
+let open = document.querySelectorAll('.open-form');
+let close = document.querySelector('.close-form');
+
+open.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        overlay.classList.add('active');
+        form.classList.add('active');
+      
+    })
+});
+
+close.addEventListener('click',() => {
+    overlay.classList.remove('active');
+    form.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === overlay) {
+        overlay.classList.remove('active');
+        form.classList.remove('active');
+    }
+});
 ////////////////Past Date Restricted///////////////////////////////////////
 let today = new Date().toISOString().split('T')[0];
 document.getElementsByName("date")[0].setAttribute('min', today);
--- museum/src/index.js
+++ museum/src/index.js
@@ -1,14 +1,12 @@
 import './components/basic-styles/style.scss'
 
-
 import './components/header/burger-menu';
 import './components/welcome-section/welcome_slider'
 import './components/explore-section/change_picture_slider'
 import './components/video-section/video-player';
 import './components/video-section/video-slider';
 import './components/gallery-section/gallery';
-import './components/tickets-section/tickets_calculator'
-import './components/tickets-section/form_validation'
-import './components/tickets-section/buy_tickets'
+import './components/tickets-section/tickets-calculator'
+import './components/tickets-section/form'
 import './components/contacts-section/interactive_map'
 
