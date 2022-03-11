4c56dda9bbbd13bb817400269a931f3eb478a870
Elizaveta <elizavetaivanushenko@gmail.com>
2022-03-11 16:09:27 +0300

    refactor: change file structure

--- museum/src/index.js
+++ museum/src/index.js
@@ -1,12 +1,12 @@
 import './components/basic-styles/style.scss'
 
 import './components/header/burger-menu';
-import './components/welcome-section/welcome_slider'
-import './components/explore-section/change_picture_slider'
+import './components/welcome-section/welcome-slider'
+import './components/explore-section/slider'
 import './components/video-section/video-player';
 import './components/video-section/video-slider';
 import './components/gallery-section/gallery';
 import './components/tickets-section/tickets-calculator'
 import './components/tickets-section/form'
-import './components/contacts-section/interactive_map'
+import './components/contacts-section/interactive-map'
 
